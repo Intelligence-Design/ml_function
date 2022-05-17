504f619c981247a8cfd9b7b92d1b45a4e6001c27
wakisaka <kentaro.wakisaka@i-d.ai>
2022-05-17 16:41:29 +0900

    first commit

---
+++ object_attribute/tflite/object_attribute_tflite/__init__.py
@@ -0,0 +1,6 @@
+DEFAULT_MODEL_DIR_PATH_DICT = {
+    'human': {
+        'S3Path': 's3://id-ai-function/v2/object_attribute/tflite/v_0_0_1_object_attribute_tflite_human.zip',
+        'VersionId': 'i.7VeD.9CqX1l3n8CDyZ8S6itWR81m5E'
+    }
+}
