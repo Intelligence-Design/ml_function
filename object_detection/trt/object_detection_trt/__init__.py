bf546a06d2ed2f9ed4673a5eed1b4eece874cb53
wakisaka <kentaro.wakisaka@i-d.ai>
2022-05-21 15:02:15 +0900

    first commit

--- object_detection/trt/object_detection_trt/__init__.py
+++ object_detection/trt/object_detection_trt/__init__.py
@@ -4,11 +4,11 @@ DEFAULT_MODEL_DIR_PATH_DICT = {
         'VersionId': 'sWM0IHuplOqpt_6hH7.auwe_PS.bRM2I'
     },
     'person_jetson_javier_nx_jp461': {
-        'S3Path': '',
-        'VersionId': ''
+        'S3Path': 's3://id-ai-function/v2/object_detection/trt/v_0_0_1_object_detection_trt_human_jetson_xavier_nx_tensorrt_8.2.1.8_jetpack_4.6.1.zip',
+        'VersionId': 'mNFsy8nV4FgSTomN_A2bV3eqYfKpDq8A'
     },
     'person_g4dn_xlarge_Deep_Learning_Base_AMI_Ubuntu_18_04_Version_44_0': {
-        'S3Path': '',
-        'VersionId': ''
+        'S3Path': 's3://id-ai-function/v2/object_detection/trt/v_0_0_1_object_detection_trt_person_person_g4dn_xlarge_Deep_Learning_Base_AMI_Ubuntu_18_04_Version_44_0.zip',
+        'VersionId': '7rcmamKTHJzWLNFFwaUytbCNrbuJ9XTw'
     },
 }
