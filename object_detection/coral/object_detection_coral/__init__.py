0b4be24db09d1d00e00cfe6fab3b2a04db27be70
wakisaka <kentaro.wakisaka@i-d.ai>
2022-05-21 20:47:12 +0900

    first commit

--- object_detection/coral/object_detection_coral/__init__.py
+++ object_detection/coral/object_detection_coral/__init__.py
@@ -1,6 +1,6 @@
 DEFAULT_MODEL_DIR_PATH_DICT = {
     'human': {
-        'S3Path': 's3://id-ai-function/v2/object_attribute/coral/v_0_0_1_object_detection_coral_human.zip',
-        'VersionId': 'ttk9xaOq5Kj22EllT7HJWvA7OH3LlDJ3'
+        'S3Path': 's3://id-ai-function/v2/object_detection/coral/v_0_0_1_object_detection_coral_human.zip',
+        'VersionId': 'x71zNFkHsa9C4SElQiEVFwwvvOJT_sHm'
     }
 }
--- object_detection/tflite/object_detection_tflite/__init__.py
+++ object_detection/tflite/object_detection_tflite/__init__.py
@@ -1,6 +1,6 @@
 DEFAULT_MODEL_DIR_PATH_DICT = {
     'human': {
         'S3Path': 's3://id-ai-function/v2/object_detection/tflite/v_0_0_1_object_detection_tflite_human.zip',
-        'VersionId': '6e8Dqvw4nZqISbbiI5xt1dn.88VUoR5E'
+        'VersionId': 'a6eOPIOYnSQj2SwSBdzLZZpBCEgmFFrc'
     }
 }
